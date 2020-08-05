a9c78837068ebd9f8f23f4b449293702e1ccff2a
Bryam P. Hernández <60350664+bryam-p@users.noreply.github.com>
2020-08-05 15:52:38 -0500

    aplicacion finalizada

--- 04-gif-expert-app/src/GifGrid.js
+++ 04-gif-expert-app/src/GifGrid.js
@@ -1,26 +1,30 @@
 import React from 'react'
-//import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from './hook/useFetchGifs'
+import { GifGridItem } from './GifGridItem';
 //import { getGifs } from './helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
-    //const [images, setImages] = useState([]);
+    const { data:images, loading } = useFetchGifs( category );
+
     /*
-    useEffect(() => { //SE USA PARA DISPARAR LAS FUNCIONES UNA SOLA VEZ
-        getGifs(category)
-            .then( imgs => setImages(imgs) )
-    }, [ category ]) //SE LE ENVIA UN ARREGLO 
-    */
+        //const [images, setImages] = useState([]);
+        useEffect(() => { //SE USA PARA DISPARAR LAS FUNCIONES UNA SOLA VEZ
+            getGifs(category)
+                .then( imgs => setImages(imgs) )
+        }, [ category ]) //SE LE ENVIA UN ARREGLO 
+        */
     return (
         <div className="contenedor">
             <h3>{category}</h3>
-            {/*
+            { loading && <p>Loading</p> }
+            {
                 images.map(img => (
                     <GifGridItem
                         key={img}
                         {...img} />
                 ))
-                */}
+                }
         </div>
     )
 }
